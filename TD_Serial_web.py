3f4bdafc2f707f13f766610ecdfe9797a3ed71b4
AKremor <AKremor@users.noreply.github.com>
2014-12-04 17:19:44 +1100

    Update TD_Serial_web.py
    Now will save all packets to file, webpage displays most recent packet. Done via threading and queuing. Removed auto-detect location try/except as it was not needed, easier to uncomment backup location.

--- TD_Serial_web.py
+++ TD_Serial_web.py
@@ -1,55 +1,48 @@
 import signal
 import sys
-from time import sleep
+from time import sleep, strftime
 from convert import convert
 import serial
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from collections import deque
+from threading import Thread
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text\html')
+        self.send_header('Content-type', 'text\html')
         self.end_headers()
-        out = get_last_serial()
-        self.wfile.write(out)
+        self.wfile.write(convert(q.popleft()[8:]))
         return
 
 
-def signal_handler(signal,frame):
+def signal_handler(signal, frame):
     # Gracefully shut down by closing serial port and web server
     print('KeyboardInterrupt, shutting down')
     ser.write('m 0\r')
     ser.close()
+    file.close()
     server.socket.close()
     sys.exit(0)
 
 
-def open_TM_serial(second_location):
+def open_TM_serial(location):
     global ser
-    first_location = '/dev/serial/by-id/usb-altusmetrum.org_TeleDongle-v0.2_000568-if00'
 
-    try:
-        ser = serial.Serial(first_location, 115200)
-    # Check what the actual exception is
-    except serial.SerialException:
-        print('Auto detect fail, attempt connection with manual location')
-        try:
-            ser = serial.Serial(second_location, 115200)
-        except:
-            print('Cannot connect with either location')
+    ser = serial.Serial(location, 115200)
 
     if ser.isOpen():
-        print('Serial connection open')
+        print('Serial connection opened')
     else:
-        print('Serial connection fail')
+        print('Serial connection failed')
         sys.exit(0)
 
-    #Throw away any old data
+    # Throw away any old data
     ser.flushInput()
     ser.flushOutput()
 
-    #Get TD to output packets
+    # Get TD to output packets
     ser.write('m 20\r')
     # Wait for the command to take effect to be safe
     sleep(2)
@@ -58,24 +51,42 @@ def open_TM_serial(second_location):
     ser.flushOutput()
 
 
-def get_last_serial():
-    data = ser.readline()
-    # Split off the 'TELEM 22' part and convert
-    return convert(data[8:])
+def serial_read(q):
+    while True:
+        recent_packet = ser.readline()
+        q.appendleft(recent_packet)
+        file.write(recent_packet)
+        print(recent_packet)
+
+
+def save_to_file():
+    filename = str(strftime("%Y%m%d-%H%M%S")) + '.txt'
+    global file
+    file = open(filename, 'w')
+    print('Logging to {0}'.format(filename))
+
 
 # Any constants
 PORT_NUMBER = 9876
 
-# Only set this if auto-detect failing
-# Unplug then plug in TeleDonge, then run dmesg to get location
-second_location = '/dev/ttyACM0'
+# If serial connection fails try other line, can find location with dmesg
+location = '/dev/serial/by-id/usb-altusmetrum.org_TeleDongle-v0.2_000568-if00'
+# location = '/dev/ttyACM0'
 
-# Open up the serial connection to TM
-open_TM_serial(device_location)
-# If we kill program then will close gracefully
+# If we kill program, close gracefully
 signal.signal(signal.SIGINT, signal_handler)
 
-server = HTTPServer(('',PORT_NUMBER),myHandler)
-print 'Started server on port', PORT_NUMBER
+open_TM_serial(location)
+
+save_to_file()
+
+server = HTTPServer(('', PORT_NUMBER), myHandler)
+print('Started server on port {0}\n'.format(PORT_NUMBER))
+
+q = deque(maxlen=100)
+# Run serial_read in its own thread
+t = Thread(target=serial_read, args=(q,))
+t.daemon = True
+t.start()
 
 server.serve_forever()
